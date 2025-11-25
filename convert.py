26f0c90dbb68308ded7346ae869d64beda2afa2c
Christoph Du <kkchristoph@outlook.com>
2025-11-26 03:10:43 +0800

    1st commitment from Windows, changed convert to Windows version and added gitignore

--- convert.py
+++ convert.py
@@ -1,3 +1,8 @@
+import os
+import sys
+os.environ['HADOOP_HOME'] = "C:\\hadoop"
+sys.path.append("C:\\hadoop\\bin")
+
 import pyspark
 from pyspark.sql import SparkSession
 
@@ -6,7 +11,7 @@ def main():
     spark = SparkSession.builder \
         .appName("DataConverter") \
         .master("local[*]") \
-        .config("spark.driver.memory", "8g") \
+        .config("spark.driver.memory", "12g") \
         .getOrCreate()
 
     print(f"Spark Web UI: {spark.sparkContext.uiWebUrl}")
